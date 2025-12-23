2dbf86cce6bc481ebe38b0cf65e83b009734ecd0
simulator <xumkox@gmail.com>
2025-12-23 17:19:17 +0100

    Update fabricacion.js

--- public/js/fabricacion.js
+++ public/js/fabricacion.js
@@ -63,14 +63,14 @@ class FabricacionManager {
             if (!produccion) return;
 
             const ahora = new Date();
-            const tiempoInicio = new Date(fab.tiempo_inicio);
+            const tiempoInicio = new Date(produccion.tiempo_inicio);
             const tiempoFin = new Date(new Date(produccion.tiempo_fin).getTime() + 3600000);
         
             const duracionTotal = tiempoFin - tiempoInicio;
             const tiempoTranscurrido = ahora - tiempoInicio;
 
             console.log('🔧 Verificación ajustada:', {
-                horaFinBD: produccion.tiempo_fin,  // <-- CAMBIADO: 'produccion', no 'fab
+                horaFinBD: produccion.tiempo_fin,
                 horaFinAjustada: tiempoFin.toISOString(),
                 duracionTotal: duracionTotal/1000 + 's',
                 tiempoTranscurrido: tiempoTranscurrido/1000 + 's',
